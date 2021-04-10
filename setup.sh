2c1895e0a19f54f7b06eeba9d8f3da30315a6211
Callum Stott <callum@seadowg.com>
2021-04-10 17:11:24 +0100

    Add beets-alternative setup

--- setup.sh
+++ setup.sh
@@ -1,12 +1,22 @@
 #! /bin/bash
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+# Install brew
+if ! command -v brew &> /dev/null
+then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+fi
+
 brew bundle
 
 # Install beets
 rm '/usr/local/bin/pip3'
 brew link python@3.7 --force
-python3 -m pip install --user beets
+if ! command -v beet &> /dev/null
+then
+  python3 -m pip install --user beets
+  python3 -m pip install --user beets-alternatives
+fi
+
 cp beets-config.yaml ~/.config/beets/config.yaml
 
 # Setup bash
