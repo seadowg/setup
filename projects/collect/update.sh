3b00291dad7daf150907e416ffec3a778a067541
Callum Stott <callum@seadowg.com>
2021-04-29 12:25:49 +0100

    Update encrv

--- projects/collect/update.sh
+++ projects/collect/update.sh
@@ -5,4 +5,5 @@ if [ -d ~/workspace/collect ]; then
   cp ~/workspace/collect/enable-animations.sh enable-animations.sh
   cp ~/workspace/collect/test-lab.sh test-lab.sh
   cp ~/workspace/collect/.local_gitignore local_gitignore
+  cp ~/workspace/collect/.envrc envrc
 fi
