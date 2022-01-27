a388c1c735bbdc71cfa4390294bbba2024104b99
Callum Stott <callum@seadowg.com>
2022-01-27 10:45:39 +0000

    Update collect stuff

--- projects/collect/test-lab.sh
+++ projects/collect/test-lab.sh
@@ -4,5 +4,5 @@ gcloud beta firebase test android run \
               --num-uniform-shards=50 \
               --app collect_app/build/outputs/apk/debug/*.apk \
               --test collect_app/build/outputs/apk/androidTest/debug/*.apk \
-              --device model=Pixel2,version=28,locale=en,orientation=portrait \
+              --device model=Pixel2,version=29,locale=en,orientation=portrait \
               --timeout 10m
