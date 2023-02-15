0afab9d55b7806e60f53ef404836a4052373a63c
Callum Stott <callum@seadowg.com>
2023-02-15 08:28:34 -0800

    Start Docker as part of setup

--- setup.sh
+++ setup.sh
@@ -18,6 +18,15 @@ source ~/.bash_profile
 brew bundle
 
 # Install beats
+printf "Starting Docker for Mac";
+open -a Docker;
+while [[ -z "$(! docker stats --no-stream 2> /dev/null)" ]];
+  do printf ".";
+  sleep 1
+done
+echo "";
+
+
 pushd beets
   docker build -t seadowg/beets .
 popd
