5573119b697f5b63a955f54d75a82b48b9c95d00
Callum Stott <callum@seadowg.com>
2023-12-18 14:24:24 +0000

    Remove test-lab.sh from update.sh

--- projects/collect/test-lab.sh
+++
@@ -1,8 +0,0 @@
-./gradlew assembleDebug assembleDebugAndroidTest
-gcloud beta firebase test android run \
-              --type instrumentation \
-              --num-uniform-shards=50 \
-              --app collect_app/build/outputs/apk/debug/*.apk \
-              --test collect_app/build/outputs/apk/androidTest/debug/*.apk \
-              --device model=MediumPhone.arm,version=30,locale=en,orientation=portrait \
-              --timeout 10m
--- projects/collect/update.sh
+++ projects/collect/update.sh
@@ -3,7 +3,6 @@
 if [ -d ~/workspace/collect ]; then
   cp ~/workspace/collect/disable-animations.sh disable-animations.sh
   cp ~/workspace/collect/enable-animations.sh enable-animations.sh
-  cp ~/workspace/collect/test-lab.sh test-lab.sh
   cp ~/workspace/collect/.local_gitignore local_gitignore
   cp ~/workspace/collect/.envrc envrc
 fi
