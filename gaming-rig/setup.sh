c7f56ee8ed7bf495d90c2efce5e3bf9800af0544
Callum Stott <callum@seadowg.com>
2025-09-08 17:06:58 +0100

    Add mangohud

--- gaming-rig/setup.sh
+++ gaming-rig/setup.sh
@@ -2,3 +2,5 @@ sudo snap install steam
 
 wget https://github.com/LizardByte/Sunshine/releases/download/v2025.628.4510/sunshine-ubuntu-24.04-amd64.deb
 sudo dpkg -i .//sunshine-ubuntu-24.04-amd64.deb
+
+sudo apt install mangohud
