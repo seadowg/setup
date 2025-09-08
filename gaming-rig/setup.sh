8fb36f8c6f0d665dd0e77c3b1e501cb8d14d1ed2
Callum Stott <callum@seadowg.com>
2025-09-08 17:50:08 +0100

    Use .deb of Steam to support mangohud

--- gaming-rig/setup.sh
+++ gaming-rig/setup.sh
@@ -1,4 +1,5 @@
-sudo snap install steam
+wget https://cdn.fastly.steamstatic.com/client/installer/steam.deb
+sudo dpkg -i steam.deb
 
 wget https://github.com/LizardByte/Sunshine/releases/download/v2025.628.4510/sunshine-ubuntu-24.04-amd64.deb
 sudo dpkg -i .//sunshine-ubuntu-24.04-amd64.deb
