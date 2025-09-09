9ef378412a47f99ae4438f00f866f0275e93c1cc
Callum Stott <callum@seadowg.com>
2025-09-09 12:16:12 +0100

    Add Heroic Launcher install

--- gaming-rig/setup.sh
+++ gaming-rig/setup.sh
@@ -11,3 +11,7 @@ sudo dpkg -i .//sunshine-ubuntu-24.04-amd64.deb
 # Install and configure MangoHud
 sudo apt install mangohud
 mkdir -p ~/.config/MangoHud/ && cp MangoHud.conf ~/.config/MangoHud/MangoHud.conf
+
+# Install Heroic launcher for non-steam games
+wget https://github.com/Heroic-Games-Launcher/HeroicGamesLauncher/releases/download/v2.18.1/Heroic-2.18.1-linux-amd64.deb
+sudo dpkg -i Heroic-2.18.1-linux-amd64.deb
