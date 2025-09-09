6b765717a5ee8ae55d9070035333eb061e4c3939
Callum Stott <callum@seadowg.com>
2025-09-09 10:16:58 +0100

    Add MangoHud config

--- gaming-rig/setup.sh
+++ gaming-rig/setup.sh
@@ -1,7 +1,11 @@
+# Install Steam
 wget https://cdn.fastly.steamstatic.com/client/installer/steam.deb
 sudo dpkg -i steam.deb
 
+# Install Sunshine
 wget https://github.com/LizardByte/Sunshine/releases/download/v2025.628.4510/sunshine-ubuntu-24.04-amd64.deb
 sudo dpkg -i .//sunshine-ubuntu-24.04-amd64.deb
 
+# Install and configure MangoHud
 sudo apt install mangohud
+cp MangoHud.conf ~/.config/MangoHud/MangoHud.conf
