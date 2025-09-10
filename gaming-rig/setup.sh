ea602b7cda710f69c06fea6434f72b987992632f
Callum Stott <callum@seadowg.com>
2025-09-10 09:53:34 +0100

    Autostart sunshine

--- gaming-rig/setup.sh
+++ gaming-rig/setup.sh
@@ -7,6 +7,7 @@ sudo dpkg -i steam.deb
 # Install Sunshine
 wget https://github.com/LizardByte/Sunshine/releases/download/v2025.628.4510/sunshine-ubuntu-24.04-amd64.deb
 sudo dpkg -i .//sunshine-ubuntu-24.04-amd64.deb
+cp sunshine.desktop ~/.config/autostart/sunshine.desktop
 
 # Install and configure MangoHud
 sudo apt install mangohud
