abe0840847a9f44f08cb4d3e197429a873a143f3
Callum Stott <callum@seadowg.com>
2025-09-11 10:52:49 +0100

    Fix path

--- gaming-rig/setup.sh
+++ gaming-rig/setup.sh
@@ -6,7 +6,7 @@ sudo dpkg -i --skip-same-version steam.deb
 
 # Install Sunshine
 wget https://github.com/LizardByte/Sunshine/releases/download/v2025.628.4510/sunshine-ubuntu-24.04-amd64.deb
-sudo dpkg -i --skip-same-version .//sunshine-ubuntu-24.04-amd64.deb
+sudo dpkg -i --skip-same-version sunshine-ubuntu-24.04-amd64.deb
 cp sunshine.desktop ~/.config/autostart/sunshine.desktop
 
 # Install and configure MangoHud
