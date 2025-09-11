ec3217aed92644b333e680378c06d1ad2a3ed7ec
Callum Stott <callum@seadowg.com>
2025-09-11 10:52:14 +0100

    Don't reinstall packages that already exist

--- gaming-rig/setup.sh
+++ gaming-rig/setup.sh
@@ -2,11 +2,11 @@ set -e
 
 # Install Steam
 wget https://cdn.fastly.steamstatic.com/client/installer/steam.deb
-sudo dpkg -i steam.deb
+sudo dpkg -i --skip-same-version steam.deb
 
 # Install Sunshine
 wget https://github.com/LizardByte/Sunshine/releases/download/v2025.628.4510/sunshine-ubuntu-24.04-amd64.deb
-sudo dpkg -i .//sunshine-ubuntu-24.04-amd64.deb
+sudo dpkg -i --skip-same-version .//sunshine-ubuntu-24.04-amd64.deb
 cp sunshine.desktop ~/.config/autostart/sunshine.desktop
 
 # Install and configure MangoHud
