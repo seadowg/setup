601b8c728da8222f8ff205d283b17a3298ab6566
Callum Stott <callum@seadowg.com>
2025-09-19 09:48:22 +0100

    Add (commented out) Heroic Launcher setup

--- gaming-rig/setup.sh
+++ gaming-rig/setup.sh
@@ -12,3 +12,7 @@ cp sunshine.desktop ~/.config/autostart/sunshine.desktop
 # Install and configure MangoHud
 sudo apt install mangohud
 mkdir -p ~/.config/MangoHud/ && cp MangoHud.conf ~/.config/MangoHud/MangoHud.conf
+
+# Install Heroic Launcher (commented out until https://github.com/Heroic-Games-Launcher/HeroicGamesLauncher/issues/4917 is fixed)
+# wget https://github.com/Heroic-Games-Launcher/HeroicGamesLauncher/releases/download/v2.18.1/Heroic-2.18.1-linux-amd64.deb
+# sudo dpkg -i --skip-same-version Heroic-2.18.1-linux-amd64.deb
