c489e1b82b1068d77faa5d4306fda42780d6252c
Callum Stott <callum@seadowg.com>
2026-01-01 11:28:55 +0000

    Use streaming host setup in rig

--- gaming-rig/setup.sh
+++ gaming-rig/setup.sh
@@ -1,20 +1,5 @@
 set -e
 
-wget -qO- https://www.seadowg.com/rig.sh | bash
-
-# Install Sunshine
-curl -O -L https://github.com/LizardByte/Sunshine/releases/download/v2025.924.154138/sunshine-ubuntu-24.04-amd64.deb
-sudo dpkg -i --skip-same-version sunshine-ubuntu-24.04-amd64.deb
-rm sunshine-ubuntu-24.04-amd64.deb
+wget -qO- https://www.seadowg.com/rig.sh | bash -s -- --streaming-host
 cp sunshine.desktop ~/.config/autostart/sunshine.desktop
-
-# Install displayconfig-mutter (for changing Sunshine stream resolutions)
-curl "https://github.com/eaglesemanation/displayconfig-mutter/releases/latest/download/displayconfig-mutter-$(uname -m)" -L -o displayconfig-mutter \
-  && sudo install -Dm0755 displayconfig-mutter /usr/local/bin/displayconfig-mutter \
-  && rm displayconfig-mutter
-
-# Install MoonDeckBuddy
-rm MoonDeckBuddy-1.9.1-x86_64.AppImage
-curl -O -L https://github.com/FrogTheFrog/moondeck-buddy/releases/download/v1.9.1/MoonDeckBuddy-1.9.1-x86_64.AppImage
-chmod +x MoonDeckBuddy-1.9.1-x86_64.AppImage
 cp MoonDeckBuddy-settings.json ~/.config/moondeckbuddy/settings.json
