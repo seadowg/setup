31d061d7e47d817265c13c0ffd3c6cf805a43b22
Callum Stott <callum@seadowg.com>
2026-01-01 11:46:27 +0000

    Use rig to configure Sunshine and MoonDeckBuddy

--- gaming-rig/setup.sh
+++ gaming-rig/setup.sh
@@ -1,5 +1,3 @@
 set -e
 
 wget -qO- https://www.seadowg.com/rig.sh | bash -s -- --streaming-host
-cp sunshine.desktop ~/.config/autostart/sunshine.desktop
-cp MoonDeckBuddy-settings.json ~/.config/moondeckbuddy/settings.json
