a4b5d023f9019ef68ff6600953ab53ef98a6caca
Callum Stott <callum@seadowg.com>
2026-01-19 12:54:59 +0000

    Comment out broken docker opening

--- homeserver/setup.sh
+++ homeserver/setup.sh
@@ -7,13 +7,13 @@ fi
 brew bundle
 
 # Install beats
-printf "Starting Docker for Mac";
-open -a Docker;
-while [[ -z "$(! docker stats --no-stream 2> /dev/null)" ]];
-  do printf ".";
-  sleep 1
-done
-echo "";
+# printf "Starting Docker for Mac";
+# open -a Docker;
+# while [[ -z "$(! docker stats --no-stream 2> /dev/null)" ]];
+  # do printf ".";
+  # sleep 1
+# done
+# echo "";
 
 
 pushd beets-docker
