3a3c96712dfb1173e5f380a5a7ac4532ef010317
Callum Stott <callum@seadowg.com>
2026-01-31 14:18:54 +0000

    Correct network adapter name for Mac Mini

--- homeserver/install-home-assistant.sh
+++ homeserver/install-home-assistant.sh
@@ -11,7 +11,7 @@ VBoxManage createvm --name "HomeAssistant" --ostype "Oracle_arm64" --register
 VBoxManage modifyvm "HomeAssistant" --memory 2048 --cpus 2
 VBoxManage modifyvm "HomeAssistant" --graphicscontroller QemuRamFB --vram 16
 VBoxManage modifyvm "HomeAssistant" --firmware efi
-VBoxManage modifyvm "HomeAssistant" --nic1 bridged --bridgeadapter1 "en0: Wi-Fi"
+VBoxManage modifyvm "HomeAssistant" --nic1 bridged --bridgeadapter1 "en1: Wi-Fi"
 VBoxManage modifyvm "HomeAssistant" --audiocontroller hda
 VBoxManage storagectl "HomeAssistant" --name "SATA Controller" --add sata --controller IntelAhci
 VBoxManage storageattach "HomeAssistant" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "haos_generic-aarch64-16.3.vdi"
